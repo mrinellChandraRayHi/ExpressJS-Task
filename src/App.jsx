1b814a8e3a3cb9d55e840f3f4ccf3d4c861c90c3
MrinellChandraRay <mrinellroy@gmail.com>
2024-04-24 13:32:22 +0600

    add react router, express js, nodemon

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react';
+
+function App() {
+
+  const [users, setUsers]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=>res.json())
+    .then(data=> setUsers(data));
+  }, [])
+
+  return (
+    <>
+      <h1>Users Management System</h1>
+      <h3>Numbers of Users: {users.length}</h3>
+    </>
+  )
+}
+
+export default App
