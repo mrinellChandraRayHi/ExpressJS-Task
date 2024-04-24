c40eac56cad43705bb943e10f7ef0a1ec11d2822
MrinellChandraRay <mrinellroy@gmail.com>
2024-04-24 15:30:42 +0600

    add to full system

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,45 @@ function App() {
     .then(data=> setUsers(data));
   }, [])
 
+  const handleAddUser=(e)=>{
+    e.preventDefault();
+    const form=e.target;
+    const name=form.name.value;
+    const email=form.email.value;
+
+    const user={name, email}
+
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method:'POST',
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      const newUsers=[...users, data]
+      setUsers(newUsers)
+      form.reset();
+    })
+  }
+
   return (
     <>
       <h1>Users Management System</h1>
       <h3>Numbers of Users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" /><br />
+        <input type="email" name="email" id="" /><br />
+        <input type="submit" value="Add user" />
+      </form>
+      <div>
+        {
+          users.map(user=> <p key={user.id}>{user.id}: {user.name}= {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
